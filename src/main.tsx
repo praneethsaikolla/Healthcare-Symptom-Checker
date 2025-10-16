e3bdc37b05cef86c62f4c8af5d3819eb9d4d09e0
Praneeth Sai Kolla <praneeth.22bce7165@vitapstudent.ac.in>
2025-10-17 00:59:50 +0530

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Create React root and mount the App
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
