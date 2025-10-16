4f94b5776852d58cd8d91f1395232a1a144c479d
Praneeth Sai Kolla <praneeth.22bce7165@vitapstudent.ac.in>
2025-10-17 00:53:47 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   server: {
     host: "::",
     port: 8080,
